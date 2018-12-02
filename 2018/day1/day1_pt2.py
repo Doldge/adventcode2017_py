4c56b94376084cb2a8c4d20864d5a208a70f9022
callum <callum@treshna.com>
2018-12-03 12:50:36 +1300

    2018 - Day 1

---
+++ 2018/day1/day1_pt2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Read the inputs
+input_list = []
+with open('./advent_2017_day_1_pt1_input.txt') as f:
+    input_list = [int(x) for x in f.readlines()]
+
+
+def find_dup_freq(current_value, seen):
+    for x in input_list:
+        current_value += x
+        if current_value in seen:
+            break
+        seen.add(current_value)
+    else:
+        current_value = find_dup_freq(current_value, seen)
+    return current_value
+
+
+# Initialization.
+current_frequency = 0
+set_of_seen_frequencies = set([current_frequency])
+
+current_frequency = find_dup_freq(current_frequency, set_of_seen_frequencies)
+
+print(current_frequency)
